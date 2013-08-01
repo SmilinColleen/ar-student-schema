598fcd689f2ef34f0d7d243dcccbb6f04ebfe0ea
sidazhang <sidazhang89@gmail.com>
2013-07-31 17:33:50 -0700

    validation magic

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,11 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, format: { with: /\S{1,}\@\S{1,}\.\S{2,}/, 
+    message: "not a valid email" }
+  validates :email, uniqueness: true
+  validates :age, numericality: { greater_than: 5 }
+  validates :phone, length: {minimum: 10}
 
   def name
     "#{first_name} #{last_name}"
@@ -10,6 +15,4 @@ class Student < ActiveRecord::Base
   def age
     ((Time.now.utc.to_date - birthday)/365.25).to_i
   end
-
-
 end
